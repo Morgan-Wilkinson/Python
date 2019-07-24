b3bad30dfc02c4eb74e8daea0d7cd7e99d35fa49
Morgan <morgan7000@hotmail.com>
2019-07-23 23:02:19 -0400

    Updates

--- Lucky.py
+++ Lucky.py
@@ -1,8 +1,31 @@
+#!/usr/bin/env python3
+
 # Morgan Wilkinson
 # 07/23/2019
 
 # I'm Lucky the worlds most magical program... Seriously though this program takes input from the
 # command line and then preforms a google search and open the top search results in another tab
 
-import os, requests, bs4
+import sys, webbrowser, requests, bs4
 
+# Prints googling while searching
+print("Googling...")
+
+# Creates the base URL for searching on google
+res = requests.get('https://www.google.com/search?q=' + ' '.join(sys.argv[1:]))
+
+# In the event of a page not being valid raise an exception 
+try:
+    res.raise_for_status()
+except Exception as exc:
+    print("Fatal Error: %s" % (exc))
+
+# Retrive the top search result links
+soup = bs4.BeautifulSoup(res.text, "html.parser")
+
+# Open a browser tab for each result
+linkElems = soup.select('div#main > div > div > div > a')
+
+numOpen = min(5, len(linkElems))
+for i in range(numOpen):
+	webbrowser.open('http://google.com' + linkElems[i].get('href'))
---
+++ lo.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+import sys, webbrowser, requests, bs4
+
+res = "http://google.com/search?q=guppu"
+webbrowser.open("http://google.com/search?q=guppu")

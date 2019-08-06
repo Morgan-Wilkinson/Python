537e62da2a10c593a4c173e284ff11f1185ca4c2
Morgan <morgan7000@hotmail.com>
2019-08-06 15:57:07 -0400

    Update downloadnovels.py

--- downloadnovels.py
+++ downloadnovels.py
@@ -5,17 +5,68 @@
 
 # This program is meant to download the novels from wuxiaworld.com
 
-import requests, sys, os, bs4, pdfkit
+import bs4, os, pdfkit, PyPDF2, requests, send2trash, sys 
 
 # Make Desktop current directory 
 os.chdir("/Users/morgan/Desktop/")
-# Ensure folder exist if not create it
+# Ensure folder exist if not create it.
 filePath = "/Users/morgan/Desktop/Spirit Realm/"
 pdfPath = filePath+"PDF/"
 
 os.makedirs(filePath, exist_ok=True)
 os.makedirs(pdfPath, exist_ok=True)
 
+# This method deletes multiple files of the same type.
+def deleteFiles(fileType):
+	for filename in os.listdir():
+		if filename.endswith(fileType):
+			print(filename)
+			send2trash.send2trash(filename)
+
+# Merge all PDFs into one PDF Book. This method retrieves ALL pdf files in the pdfPath and combines them.
+# If you want only what was recently downloaded run the deletFiles function first.
+def pdfMerge(pdfPath):
+	# Move current directory to /Users/morgan/Desktop/Spirit Realm/PDF/
+	os.chdir(pdfPath)
+
+	# Deletes the file if it exists to avoid a invalid stream length error.
+	if os.path.isfile(pdfPath+"Spirit Realm.pdf") == True:
+		send2trash.send2trash("Spirit Realm.pdf")
+
+	# Create an empty list
+	pdfFiles = []
+	# Search in the current directory for files ending with .pdf
+	for filename in os.listdir('.'):
+		if filename.endswith(".pdf"):
+			# Add all found files to the list.
+			pdfFiles.append(filename)
+	# Sort the list
+	pdfFiles.sort(key=str.lower)
+	# Create a pdfFileWriter 
+	pdfWriter = PyPDF2.PdfFileWriter()
+
+	index = 1
+	# For each file in the pdfFiles list open it and create a pdfFileReader object.
+	for filename in pdfFiles:
+		pdfFileObj = open(filename, 'rb')
+		pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+
+		for pageNum in range(0, pdfReader.numPages):
+			# Add all found pdf pages to the pdfWriter object.
+			pageObj = pdfReader.getPage(pageNum)
+			pdfWriter.addPage(pageObj)
+		
+		# Offset to account of pageNum starting at 0.
+		offset = 1
+		# Bookmark for each page is chapter is calculated by taking the total number of pages and subtracting it from the chapter length.
+		bookmarkPage = pdfWriter.getNumPages() - offset - pageNum
+		pdfWriter.addBookmark("Chapter "+str(index), bookmarkPage)
+		index += 1
+
+	# Save the results.
+	pdfOutput = open('Spirit Realm.pdf', 'wb')
+	pdfWriter.write(pdfOutput)
+	pdfOutput.close()
 
 # Convert files in path folder
 def convertToPdf(filePath, pdfPath):
@@ -25,6 +76,10 @@ def convertToPdf(filePath, pdfPath):
 			output = (os.path.splitext(filename)[0])
 			pdfkit.from_file(filePath+filename, pdfPath+output+".pdf")
 
+	# Call pdfMerge once all files have been converted to pdf
+	pdfMerge(pdfPath)
+
+# Get chapter content and process it.
 def chapters():
 	# Starting url
 	url = "https://www.wuxiaworld.com/novel/spirit-realm/sr-chapter-1"
@@ -33,7 +88,7 @@ def chapters():
 	if len(sys.argv) > 1:
 		desired = ' '.join(sys.argv[1:])
 	else:
-		desired = 10
+		desired = 5
 
 	index = 1
 	while not index == int(desired):
@@ -83,6 +138,7 @@ def chapters():
 
 	convertToPdf(filePath, pdfPath)
 
+# Starts off the process. The root basically.
 chapters()
 
 
